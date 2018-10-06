a285940a5adaf6018c7b917792097715a2af5279
Vy <vuvykt38@gmail.com>
2018-10-06 22:13:39 +0800

    delete text column

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,7 @@ ActiveRecord::Schema.define(version: 2018_10_06_082025) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
-    t.string "text"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
