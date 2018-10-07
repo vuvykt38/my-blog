b364c06ccb717037c86704515ede1341b6aace3f
Vy <vuvykt38@gmail.com>
2018-10-07 14:37:47 +0800

    create new model Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'wellcome/index'
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_082025) do
+ActiveRecord::Schema.define(version: 2018_10_07_062924) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
