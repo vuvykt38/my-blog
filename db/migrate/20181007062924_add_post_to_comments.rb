c96d7bb6109c5f96c74f31c6fa9d540ca1f24dc5
Vy <vuvykt38@gmail.com>
2018-10-07 17:21:34 +0800

    add delete for comment

--- db/migrate/20181007062924_add_post_to_comments.rb
+++ db/migrate/20181007062924_add_post_to_comments.rb
@@ -1,5 +1,5 @@
 class AddPostToComments < ActiveRecord::Migration[5.2]
   def change
-    add_reference :comments, :post, foreign_key: true
+    add_reference :comments, :post, foreign_key: true, index: true
   end
 end
