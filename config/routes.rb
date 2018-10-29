b36142dd913d5e8e28707c73ee7d65761df5ccd8
Vy <vuvykt38@gmail.com>
2018-10-30 00:50:40 +0800

    remove welcome page, route to new page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  get 'wellcome/index'
   resources :posts do
     resources :comments
   end
 
   resources :profiles, only: [:show, :edit, :update]
 
-  root 'welcome#index'
+  root 'posts#index'
 end
