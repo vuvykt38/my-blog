38efeee2844079c8821b5d2c207f24cd91dd36a9
Vy <vuvykt38@gmail.com>
2018-11-11 23:40:27 +0800

    create Relationship model

---
+++ db/migrate/20181111153506_add_unique_index_on_follower_id_and_followed_id_to_relationships.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexOnFollowerIdAndFollowedIdToRelationships < ActiveRecord::Migration[5.2]
+  def change
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_135420) do
+ActiveRecord::Schema.define(version: 2018_11_11_153506) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -74,6 +74,16 @@ ActiveRecord::Schema.define(version: 2018_11_01_135420) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "follower_id"
+    t.bigint "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,4 +107,6 @@ ActiveRecord::Schema.define(version: 2018_11_01_135420) do
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
+  add_foreign_key "relationships", "users", column: "followed_id"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end
