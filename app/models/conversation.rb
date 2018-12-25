1f33e779241e4758116c98dceef2da0d687d344d
Vy <vuvykt38@gmail.com>
2018-12-25 14:24:04 +0800

    update other_user method to use new models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,8 +1,19 @@
 class Conversation < ApplicationRecord
   has_many :private_messages
   has_many :user_conversations
+  has_many :users, through: :user_conversations
+
+
+  # TODO:  remove first_participate and second_participate when everyone migrated
   belongs_to :first_participate, class_name: 'User'
   belongs_to :second_participate, class_name: 'User'
+
+  def self.conversation_for(user_id, other_id)
+    Conversation.find_or_create_by(first_participate_id: [user_id, other_id].min,
+                                   second_participate_id: [user_id, other_id].max)
+  end
+  ##################################################################################################
+
   belongs_to :last_message, class_name: 'PrivateMessage', foreign_key: 'last_message_id', optional: true
 
   scope :conversations_of, lambda { |user|
@@ -12,13 +23,8 @@ class Conversation < ApplicationRecord
 
   scope :unread, -> { where('user_conversations.unread = ?', true) }
 
-  def self.conversation_for(user_id, other_id)
-    Conversation.find_or_create_by(first_participate_id: [user_id, other_id].min,
-                                   second_participate_id: [user_id, other_id].max)
-  end
-
   def other_user(current_user)
-    current_user == first_participate ? second_participate : first_participate
+    (users.to_a - [current_user]).first
   end
 
   def message_time
