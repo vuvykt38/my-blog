c9e97b459f3b88608a739ad4d835f814a6c3191f
Vy <vuvykt38@gmail.com>
2019-01-02 23:07:49 +0800

    use ffaker to edit user factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,9 @@
+require 'ffaker'
+
 FactoryBot.define do
   factory :user do
-    email { 'vuvy@example.com' }
-    full_name { 'Vu Vy' }
+    email { FFaker::Internet.email }
+    password '123456'
+    full_name { FFaker::Name.name }
   end
 end
